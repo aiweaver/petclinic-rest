39d22ad4d4815c197a36f4f7572a9cbd4271e143
EC2 Default User <ec2-user@ip-172-31-39-3.ap-southeast-1.compute.internal>
2018-06-16 07:04:53 +0000

    수정 #3

--- src/main/groovy/vw/demo/petclinic/interfaces/PetClinicController.groovy
+++ src/main/groovy/vw/demo/petclinic/interfaces/PetClinicController.groovy
@@ -23,6 +23,6 @@ class PetClinicController {
 
     @GetMapping("/developer")
     ResponseEntity getDeveloper(){
-        return ResponseEntity.ok("AI Weaver")
+        return ResponseEntity.ok("AI Weaver Inc.")
     }
 }
